f18adeb1f3b745b86a68744019be0805deb17119
Joe Greene <clydemachine@gmail.com>
2016-12-31 13:42:42 -0800

    Introduce intent handling, reprompt text functionality.

--- src/main/scala/com/clydemachine/alexabulbapedia/AlexaBulbaSpeechlet.scala
+++ src/main/scala/com/clydemachine/alexabulbapedia/AlexaBulbaSpeechlet.scala
@@ -1,5 +1,6 @@
 package com.clydemachine.alexabulbapedia
 
+import com.amazon.speech.slu.Intent
 import com.amazon.speech.speechlet._
 import com.clydemachine.alexabulbapedia.speech.speechlet.SpeechletResponse
 import com.clydemachine.alexabulbapedia.speech.ui.PlainTextOutputSpeech
@@ -19,11 +20,31 @@ class AlexaBulbaSpeechlet extends Speechlet {
 
   override def onIntent(request: IntentRequest, session: Session): SpeechletResponse = {
     logInvocation("onIntent", request, session)
-    // This is where we actually handle the user's question and provide a response.
-    val ReceivedPokeName = "abra".toLowerCase
-    val PokeEvolutionInfoAnswer = AlexaBulba.getEvolutionDetails(ReceivedPokeName)
-    val outputSpeech = new PlainTextOutputSpeech(PokeEvolutionInfoAnswer)
-    new SpeechletResponse(outputSpeech)
+
+    val intent: Intent = request.getIntent
+    val intentName: String = intent.getName
+
+    // This log is for debugging purposes only.
+    logInvocation(s"onIntent detected intent: $intentName", request, session)
+
+    if (intentName == "GetPokemonEvolution") {
+      // This is where we actually handle the user's question and provide a response.
+      val ReceivedPokeName = "abra".toLowerCase
+      val PokeEvolutionInfoAnswer = AlexaBulba.getEvolutionDetails(ReceivedPokeName)
+      val outputSpeech = new PlainTextOutputSpeech(PokeEvolutionInfoAnswer)
+      new SpeechletResponse(outputSpeech)
+    } else if (intentName == "AMAZON.HelpIntent") {
+      val outputSpeech = new PlainTextOutputSpeech(
+        """
+          | This skill gives evolution information for almost all Poke'mon in existence.
+          | Ask something like, what does Abra evolve into, and I'll give you what I know.
+        """.stripMargin)
+      val repromptText = "Which Poke'mon would you like to know about? "
+      new SpeechletResponse(outputSpeech, repromptText)
+    } else {
+      val outputSpeech = new PlainTextOutputSpeech("Until next time! ")
+      new SpeechletResponse(outputSpeech)
+    }
   }
 
   override def onSessionEnded(request: SessionEndedRequest, session: Session): Unit = {
--- src/main/scala/com/clydemachine/alexabulbapedia/speech/speechlet/SpeechletResponse.scala
+++ src/main/scala/com/clydemachine/alexabulbapedia/speech/speechlet/SpeechletResponse.scala
@@ -2,13 +2,20 @@ package com.clydemachine.alexabulbapedia.speech.speechlet
 
 import com.amazon.speech.speechlet
 import com.amazon.speech.ui.{Card, OutputSpeech, Reprompt}
+import com.clydemachine.alexabulbapedia.speech.ui.PlainTextOutputSpeech
 
 class SpeechletResponse(
                        outputSpeech: OutputSpeech = null,
+                       repromptText: String = "",
                        card: Card = null,
-                       reprompt: Reprompt = null,
                        shouldEndSession: Boolean = true
                        ) extends speechlet.SpeechletResponse {
+
+  // Handle reprompt text, convert to Reprompt type before passing.
+  var repromptOutputSpeech: OutputSpeech = new PlainTextOutputSpeech(repromptText)
+  var reprompt: Reprompt = new Reprompt
+  reprompt.setOutputSpeech(repromptOutputSpeech)
+
   setOutputSpeech(outputSpeech)
   setCard(card)
   setReprompt(reprompt)
