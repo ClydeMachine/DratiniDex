27386a9351b986502861a0f0e2c95a05b1e5eebc
Joe Greene <clydemachine@gmail.com>
2016-12-31 14:17:26 -0800

    Update SampleUtterances.

--- src/main/java/com/clydemachine/alexabulbapedia/AlexaBulbaSpeechletRequestStreamHandler.java
+++ src/main/java/com/clydemachine/alexabulbapedia/AlexaBulbaSpeechletRequestStreamHandler.java
@@ -8,13 +8,13 @@ import java.util.HashSet;
 import java.util.Set;
 
 public class AlexaBulbaSpeechletRequestStreamHandler extends SpeechletRequestStreamHandler {
-    private static final Set<String> appIds = new HashSet<String>();
-    static {
-        //Add new Application IDs that should be allowed to use this skill with: appIds.add("");
-        appIds.add("");
-    }
+  private static final Set<String> appIds = new HashSet<String>();
+  static {
+    //Add new Application IDs that should be allowed to use this skill with: appIds.add("");
+    appIds.add("amzn1.ask.skill.38b4a620-6c5d-4aed-aec0-7ad1831cb18c");
+  }
 
   public AlexaBulbaSpeechletRequestStreamHandler() {
-      super( new AlexaBulbaSpeechlet(), appIds );
+    super( new AlexaBulbaSpeechlet(), appIds );
   }
 }
