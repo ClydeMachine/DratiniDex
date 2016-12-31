b82ddffef3576538ec2bcc7504d2ce0dfcdf7867
Joe Greene <clydemachine@gmail.com>
2016-12-31 12:05:25 -0800

    Remove all Alexa resources to start building them up from scratch.

--- src/main/scala/LogicTester.scala
+++ src/main/scala/LogicTester.scala
@@ -2,9 +2,8 @@ package com.clydemachine.alexabulbapedia
 
 object LogicTester extends AlexaBulba {
   def main(args: Array[String]): Unit = {
-    // Run tests of the above logic. Later we'll integrate it into the Alexa skill structure.
+    // Run tests of the application logic. This is for dev testing before Alexa integration.
     // Test with kangaskhan for no evolutions, abra for a pretty standard evolution, gengar for a final evolution.
     println(getEvolutionDetails("dratini"))
-    println(getEvolutionDetails("tyranitar"))
   }
 }
--- src/main/scala/PlainTextOutputSpeech.scala
+++
@@ -1,6 +0,0 @@
-package com.clydemachine.alexabulbapedia
-/*
-class PlainTextOutputSpeech(text: String) extends ui.PlainTextOutputSpeech {
-  setText(text)
-}
-*/
