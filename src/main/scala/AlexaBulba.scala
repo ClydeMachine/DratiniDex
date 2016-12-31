c298cfcc19617c3fae7cbfdc588d0dad98a24b6d
Joe Greene <clydemachine@gmail.com>
2016-12-31 03:07:50 -0800

    Able to retrieve and parse JSON data from PokeAPI. Now to add logic based on returned information.

--- src/main/scala/AlexaBulba.scala
+++ src/main/scala/AlexaBulba.scala
@@ -3,6 +3,8 @@ package com.clydemachine.alexabulbapedia
 import java.io.IOException
 import java.net.SocketTimeoutException
 
+import play.api.libs.json._
+
 import com.amazon.speech.slu.Intent
 import com.amazon.speech.speechlet.{
   IntentRequest,
@@ -26,21 +28,17 @@ import com.amazon.speech.speechlet.{
  */
 
 class AlexaBulba {
-  val endpointGetPokeName = "http://pokeapi.co/api/v2/pokemon/"
+
+  val endpointGetPokeName = "http://pokeapi.co/api/v2/pokemon-species/"
   val endpointGetEvolInfo = "http://pokeapi.co/api/v2/evolution-chain/"
 
-  //def get(url: String) = io.Source.fromURL(url).mkString
-  def get(url: String,
-          connectTimeout: Int = 5000,
-          readTimeout: Int = 5000,
-          requestMethod: String = "GET") =
-  {
+  def get(url: String, connectTimeout: Int = 5000, readTimeout: Int = 5000, requestMethod: String = "GET"): String =  {
     import java.net.{URL, HttpURLConnection}
     val connection = new URL(url).openConnection.asInstanceOf[HttpURLConnection]
     connection.setConnectTimeout(connectTimeout)
     connection.setReadTimeout(readTimeout)
     connection.setRequestMethod(requestMethod)
-    connection.addRequestProperty("User-Agent", "OPR/42.0.2393.94")
+    connection.addRequestProperty("User-Agent", "OPR/42.0.2393.94") // Should probably make a unique User-Agent string for this application.
     val inputStream = connection.getInputStream
     val content = io.Source.fromInputStream(inputStream).mkString
     if (inputStream != null) inputStream.close
@@ -49,13 +47,15 @@ class AlexaBulba {
 
   def getEvolutionDetails(pokename: String): String = {
     try {
-      println(s"Got $pokename, GETting ID...")
-      val pokeID = get(endpointGetPokeName + pokename)
-      println(s"Got response! $pokeID \n\nFetching evolution information...")
-      // Assume pokeID is extracted...
-      val evolInfo = get(endpointGetEvolInfo + "pokeID")
-      return evolInfo.mkString("\n")
+      val PokemonSpeciesDetails = Json.parse(get(endpointGetPokeName + pokename))
+      val PokemonEvolutionChainURL = ((PokemonSpeciesDetails \ "evolution_chain" \ "url").get).as[String]
+      println(s"Got evolution chain, fetching from $PokemonEvolutionChainURL...")
+      val PokemonEvolutionChainDetails = Json.parse(get(PokemonEvolutionChainURL))
 
+      // Does this evolution chain contain evolution stages?
+
+
+      return "Evolution information result returned."
     } catch {
       case ioexception: IOException => return s"IOException! $ioexception"
       case timeout: SocketTimeoutException => return s"Request timed out! $timeout"
