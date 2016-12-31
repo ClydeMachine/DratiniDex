8abe24d4ece5c5ebc7ef4b9c3a6828e0318da83d
Joe Greene <clydemachine@gmail.com>
2016-12-31 14:37:45 -0800

    Update RequestStreamHandler AppIds. Include Icon image for Alexa Skill.

--- src/main/java/com/clydemachine/alexabulbapedia/AlexaBulbaSpeechletRequestStreamHandler.java
+++ src/main/java/com/clydemachine/alexabulbapedia/AlexaBulbaSpeechletRequestStreamHandler.java
@@ -11,6 +11,7 @@ public class AlexaBulbaSpeechletRequestStreamHandler extends SpeechletRequestStr
   private static final Set<String> appIds = new HashSet<String>();
   static {
     //Add new Application IDs that should be allowed to use this skill with: appIds.add("");
+    appIds.add("amzn1.ask.skill.d2bfbe87-e22d-4cff-b9fe-b06c31120a49");
     appIds.add("amzn1.ask.skill.38b4a620-6c5d-4aed-aec0-7ad1831cb18c");
   }
 
